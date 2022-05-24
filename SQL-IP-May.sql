316919a791e93a2051349c524ff699791113ec58
kuchun629 <ruby1453423@gmail.com>
2022-05-25 00:48:17 +0100

    Update SQL-IP-May.sql

--- SQL-IP-May.sql
+++ SQL-IP-May.sql
@@ -4,12 +4,14 @@ UNION
 SELECT column_name(s) FROM table_name2;
 
 # 2. Left Join (not including the overlapping part)
+
 # mysolution
 SELECT name AS Customers
 FROM Customers
 LEFT JOIN Orders 
 ON Customers.id = Orders.customerId
 WHERE Orders.customerId IS NULL
+
 # LeetCode solution
 select customers.name as 'Customers'
 from customers
@@ -20,7 +22,7 @@ where customers.id not in
 
 # 3. CASE WHEN
 SELECT employee_id,
-			 CASE WHEN employee_id % 2 != 0 AND name NOT LIKE 'M%' THEN salary
-       ELSE 0 END AS bonus
+		CASE WHEN employee_id % 2 != 0 AND name NOT LIKE 'M%' THEN salary
+        ELSE 0 END AS bonus
 FROM Employees;
 
